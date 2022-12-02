daf3aff942d54ad7e1db6a2461226fc051283ca1
Novák Viktor <novk.viktor@gmail.com>
2022-12-02 06:17:24 +0100

    day2 part 2

--- src/com/adventofcode/day2/Puzzle.java
+++ src/com/adventofcode/day2/Puzzle.java
@@ -6,7 +6,7 @@ import static com.adventofcode.helper.InputReaderUtil.createListFromFile;
 
 public class Puzzle {
 
-    public static void main(String[] args) {
+    public static void mainOld(String[] args) {
         List<String> input = createListFromFile("src/com/adventofcode/day2/input.txt");
 
         int score = 0;
@@ -39,6 +39,46 @@ public class Puzzle {
                     score += 2 + 0;
                 } else {
                     score += 3 + 3;
+                }
+            }
+        }
+
+        System.out.println(score);
+    }
+
+    public static void main(String[] args) {
+        List<String> input = createListFromFile("src/com/adventofcode/day2/input.txt");
+
+        int score = 0;
+
+        for (String match : input) {
+            String[] hands = match.split(" ");
+            String enemy = hands[0];
+            String me = hands[1];
+
+            if (enemy.equals("A")) {
+                if (me.equals("X")) {
+                    score += 3 + 0;
+                } else if (me.equals("Y")) {
+                    score += 1 + 3;
+                } else {
+                    score += 6 + 2;
+                }
+            } else if (enemy.equals("B")) {
+                if (me.equals("X")) {
+                    score += 1 + 0;
+                } else if (me.equals("Y")) {
+                    score += 2 + 3;
+                } else {
+                    score += 6 + 3;
+                }
+            } else {
+                if (me.equals("X")) {
+                    score += 2 + 0;
+                } else if (me.equals("Y")) {
+                    score += 3 + 3;
+                } else {
+                    score += 6 + 1;
                 }
             }
         }
