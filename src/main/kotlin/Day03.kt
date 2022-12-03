ecf01f6b38d6a2be4e97a7934352819082df8416
Novák Viktor <novk.viktor@gmail.com>
2022-12-03 19:52:19 +0100

    add abstract class

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,64 +1,65 @@
-data class Backpack(val firstRucksack : String, val secondRucksack: String)
+object Day03 : DayXX() {
+    private data class Backpack(val firstRucksack : String, val secondRucksack: String)
 
-data class Group(val firstBackpack: String, val secondBackpack: String, val thirdBackpacks: String)
+    private data class Group(val firstBackpack: String, val secondBackpack: String, val thirdBackpacks: String)
 
-fun part1() {
-    var score = 0
-    val inputs = readInput("day03")
-    val backpacks = ArrayList<Backpack>()
+     override fun part1() {
+        var score = 0
+        val inputs = readInput("day03")
+        val backpacks = ArrayList<Backpack>()
 
-    for (inputLine in inputs) {
-        val inputLength = inputLine.length
-        backpacks.add(Backpack(inputLine.substring(0, inputLength/2), inputLine.substring(inputLength/2, inputLength)))
-    }
-
-    for (backpack in backpacks) {
-        val firstRucksack = backpack.firstRucksack.toCharSet()
-        val secondRucksack = backpack.secondRucksack.toCharSet()
-
-
-        val intersect = firstRucksack.intersect(secondRucksack)
-
-        score += intersect.first().getValue()
-    }
-
-    println(score)
-}
-
-fun part2() {
-    var score = 0
-    val inputs = readInput("day03")
-    val groups = ArrayList<Group>()
-
-    for (i in 0..inputs.size - 3 step 3) {
-        groups.add(Group(inputs[i], inputs[i + 1], inputs[i + 2]))
-    }
-
-    for (group in groups) {
-        val firstBackpack = group.firstBackpack.toCharSet()
-        val secondBackpack = group.secondBackpack.toCharSet()
-        val thirdBackpacks = group.thirdBackpacks.toCharSet()
-
-        val intersect = firstBackpack.intersect(secondBackpack).intersect(thirdBackpacks)
-
-        score += intersect.first().getValue()
-    }
-
-    println(score)
-}
-
-fun String.toCharSet() = toCharArray().toSet()
-
-fun Char.getValue(): Int =
-    let {
-        if (it.isLowerCase()) {
-            return it.code - 96
-        } else {
-            return it.code - 65 + 27
+        for (inputLine in inputs) {
+            val inputLength = inputLine.length
+            backpacks.add(Backpack(inputLine.substring(0, inputLength/2), inputLine.substring(inputLength/2, inputLength)))
         }
+
+        for (backpack in backpacks) {
+            val firstRucksack = backpack.firstRucksack.toCharSet()
+            val secondRucksack = backpack.secondRucksack.toCharSet()
+
+
+            val intersect = firstRucksack.intersect(secondRucksack)
+
+            score += intersect.first().getValue()
+        }
+
+        println(score)
     }
+
+    override fun part2() {
+        var score = 0
+        val inputs = readInput("day03")
+        val groups = ArrayList<Group>()
+
+        for (i in 0..inputs.size - 3 step 3) {
+            groups.add(Group(inputs[i], inputs[i + 1], inputs[i + 2]))
+        }
+
+        for (group in groups) {
+            val firstBackpack = group.firstBackpack.toCharSet()
+            val secondBackpack = group.secondBackpack.toCharSet()
+            val thirdBackpacks = group.thirdBackpacks.toCharSet()
+
+            val intersect = firstBackpack.intersect(secondBackpack).intersect(thirdBackpacks)
+
+            score += intersect.first().getValue()
+        }
+
+        println(score)
+    }
+
+    private fun String.toCharSet() = toCharArray().toSet()
+
+    private fun Char.getValue(): Int =
+        let {
+            if (it.isLowerCase()) {
+                return it.code - 96
+            } else {
+                return it.code - 65 + 27
+            }
+        }
+}
 
 fun main() {
-    part1()
-    part2()
+    Day03.solve()
 }
---
+++ src/main/kotlin/DayXX.kt
@@ -0,0 +1,9 @@
+abstract class DayXX {
+    protected abstract fun part1()
+    protected abstract fun part2()
+
+    fun solve() {
+        part1()
+        part2()
+    }
+}
