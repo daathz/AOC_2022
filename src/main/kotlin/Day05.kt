fa0641a99eb83f019188933416ab6f2ae3f84878
Novák Viktor <novk.viktor@gmail.com>
2022-12-05 07:02:08 +0100

    day05 part2

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -44,7 +44,43 @@ object Day05 : DayXX() {
     }
 
     override fun part2() {
+        val inputs = readInput("day05")
 
+        val crates = inputs.takeWhile { it != "" }.reversed()
+        val rearrangements = inputs.takeLastWhile { it != "" }
+
+        val stacks = ArrayList<ArrayDeque<Char>>()
+        for (column in 0 until crates[0].length) {
+            stacks.add(ArrayDeque())
+        }
+
+        for (line in crates) {
+            for ((index, column) in line.withIndex()) {
+                if (column != ' ') {
+                    stacks[index].add(column)
+                }
+            }
+        }
+
+        val moves = rearrangements.map {
+            val tokens = it.split(" ")
+            Move(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[5]))
+        }
+
+        for (move in moves) {
+            val temp = ArrayDeque<Char>()
+            for (i in 0 until move.size) {
+                val last = stacks[move.from - 1].last()
+                stacks[move.from - 1].removeLast()
+                temp.add(last)
+            }
+            stacks[move.to - 1].addAll(temp.reversed())
+        }
+
+        for (stack in stacks) {
+            print(stack.last)
+        }
+        println()
     }
 }
 
