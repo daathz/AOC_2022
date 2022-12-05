dabc35bd38eb581458dd8303836a1103357e6b4b
Novák Viktor <novk.viktor@gmail.com>
2022-12-05 08:19:38 +0100

    day05 refactor

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -6,46 +6,18 @@ object Day05 : DayXX() {
     private data class Move(val size: Int, val from: Int, val to: Int)
 
     override fun part1() {
-        val inputs = readInput("day05")
+        val (stacks, moves) = createStacksAndMoves(readInput("day05"))
 
-        val crates = inputs.takeWhile { it != "" }.reversed()
-        val rearrangements = inputs.takeLastWhile { it != "" }
-
-        val stacks = ArrayList<ArrayDeque<Char>>()
-        for (column in 0 until crates[0].length) {
-            stacks.add(ArrayDeque())
-        }
-
-        for (line in crates) {
-            for ((index, column) in line.withIndex()) {
-                if (column != ' ') {
-                    stacks[index].add(column)
-                }
-            }
-        }
-
-        val moves = rearrangements.map {
-            val tokens = it.split(" ")
-            Move(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[5]))
-        }
-
-        for (move in moves) {
-            for (i in 0 until move.size) {
-                val last = stacks[move.from - 1].last()
-                stacks[move.from - 1].removeLast()
-                stacks[move.to - 1].add(last)
-            }
-        }
-
-        for (stack in stacks) {
-            print(stack.last)
-        }
-        println()
+        doRearrangements(stacks, moves)
     }
 
     override fun part2() {
-        val inputs = readInput("day05")
+        val (stacks, moves) = createStacksAndMoves(readInput("day05"))
 
+        doRearrangements(stacks, moves, true)
+    }
+
+    private fun createStacksAndMoves(inputs: List<String>): Pair<ArrayList<ArrayDeque<Char>>, List<Move>> {
         val crates = inputs.takeWhile { it != "" }.reversed()
         val rearrangements = inputs.takeLastWhile { it != "" }
 
@@ -54,8 +26,8 @@ object Day05 : DayXX() {
             stacks.add(ArrayDeque())
         }
 
-        for (line in crates) {
-            for ((index, column) in line.withIndex()) {
+        crates.forEach {
+            for ((index, column) in it.withIndex()) {
                 if (column != ' ') {
                     stacks[index].add(column)
                 }
@@ -67,19 +39,23 @@ object Day05 : DayXX() {
             Move(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[5]))
         }
 
-        for (move in moves) {
+        return Pair(stacks, moves)
+    }
+
+    private fun doRearrangements(stacks: ArrayList<ArrayDeque<Char>>, moves: List<Move>, batch: Boolean = false) {
+        moves.forEach {
             val temp = ArrayDeque<Char>()
-            for (i in 0 until move.size) {
-                val last = stacks[move.from - 1].last()
-                stacks[move.from - 1].removeLast()
+            for (i in 0 until it.size) {
+                val last = stacks[it.from - 1].last()
+                stacks[it.from - 1].removeLast()
                 temp.add(last)
             }
-            stacks[move.to - 1].addAll(temp.reversed())
+            stacks[it.to - 1].addAll(
+                if (batch) temp.reversed() else temp
+            )
         }
 
-        for (stack in stacks) {
-            print(stack.last)
-        }
+        stacks.forEach { print(it.last) }
         println()
     }
 }
