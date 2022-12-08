bd2ea3ab9086820c9dba9487871c04f680b73460
Novák Viktor <novk.viktor@gmail.com>
2022-12-08 14:31:15 +0100

    day08 refactor

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,7 +1,46 @@
 object Day08 : DayXX() {
     override fun part1() {
-        val forest = mutableListOf<MutableList<Int>>()
-        readInput("day08").forEach {
+        val forest = getForest(readInput("day08"))
+
+        var visibleForests = 0
+        for ((ri, row) in forest.withIndex()) {
+            for ((ci, _) in row.withIndex()) {
+                if (checkNTrees(forest, ri, ci, "", forest[ri][ci])) {
+                    visibleForests++
+                }
+            }
+        }
+
+        println(visibleForests)
+    }
+
+    override fun part2() {
+        val forest = getForest(readInput("day08"))
+
+        var score = 0
+
+        forest.withIndex().forEach { (ri, row) ->
+            row.withIndex().forEach { (ci, _) ->
+                val current = forest[ri][ci]
+
+                val topScore = checkTopScore(forest, ri, ci, current)
+                val bottomScore = checkBottomScore(forest, ri, ci, current)
+                val leftScore = checkLeftScore(forest, ri, ci, current)
+                val rightScore = checkRightScore(forest, ri, ci, current)
+
+                val currentScore = topScore * bottomScore * leftScore * rightScore
+
+                score = maxOf(score, currentScore)
+            }
+        }
+
+        println(score)
+    }
+
+    private fun getForest(input: List<String>): MutableList<MutableList<Int>> {
+        val forest =  mutableListOf<MutableList<Int>>()
+
+        input.forEach {
             val arr = mutableListOf<Int>()
             it.forEach {c ->
                 arr.add(Integer.parseInt(c.toString()))
@@ -9,56 +48,42 @@ object Day08 : DayXX() {
             forest.add(arr)
         }
 
-        var score = 0
-        for ((ri, row) in forest.withIndex()) {
-            for ((ci, _) in row.withIndex()) {
-                val current = forest[ri][ci]
-                val topScore = checkTopScore(forest, ri, ci, current)
-                val bottomScore = checkBottomScore(forest, ri, ci, current)
-                val leftScore = checkLeftScore(forest, ri, ci, current)
-                val rightScore = checkRightScore(forest, ri, ci, current)
-                val currentScore = topScore * bottomScore * leftScore * rightScore
-                score = maxOf(score, currentScore)
-            }
-            println()
-        }
-        println(score)
+        return forest
     }
 
     private fun checkNTrees(list: MutableList<MutableList<Int>>, i: Int, j: Int, direction: String, original: Int): Boolean {
-        var result = false
-        val current = list.get(i).get(j)
+        var result: Boolean
 
         if (i == 0 || j == 0 || i + 1 == list.size || j + 1 == list[0].size) return true
 
-        if (list.get(i - 1).get(j) < original /*&& list.get(i - 1).get(j) <= current*/ && (direction == "top" ||
+        if (list[i - 1][j] < original && (direction == "top" ||
                     direction == "")) {
             result = checkNTrees(list, i - 1, j, "top", original)
             if (result) return true
         }
-        if (list.get(i + 1).get(j) < original /*&& list.get(i + 1).get(j) <= current*/ && (direction == "bottom" ||
+        if (list[i + 1][j] < original && (direction == "bottom" ||
                     direction == "")) {
             result = checkNTrees(list, i + 1, j, "bottom", original)
             if (result) return true
         }
-        if (list.get(i).get(j - 1) < original /*&& list.get(i).get(j - 1) <= current*/ && (direction == "left" ||
+        if (list[i][j - 1] < original && (direction == "left" ||
                     direction == "")) {
             result = checkNTrees(list, i, j - 1, "left", original)
             if (result) return true
         }
-        if (list.get(i).get(j + 1) < original /*&& list.get(i).get(j + 1) <= current*/ && (direction == "right" ||
+        if (list[i][j + 1] < original && (direction == "right" ||
                     direction == "")) {
             result = checkNTrees(list, i, j + 1, "right", original)
             if (result) return true
         }
 
-        return result
+        return false
     }
 
     private fun checkTopScore(list: MutableList<MutableList<Int>>, i: Int, j: Int, original: Int): Int {
-        val previousHeights = mutableListOf<Int>()
-
         if (i == 0) return 0
+
+        val previousHeights = mutableListOf<Int>()
 
         for (idx in i - 1 downTo 0) {
             val current = list[idx][j]
@@ -74,9 +99,9 @@ object Day08 : DayXX() {
     }
 
     private fun checkBottomScore(list: MutableList<MutableList<Int>>, i: Int, j: Int, original: Int): Int {
-        val previousHeights = mutableListOf<Int>()
-
         if (i + 1 == list.size) return 0
+
+        val previousHeights = mutableListOf<Int>()
 
         for (idx in i + 1 until list.size) {
             val current = list[idx][j]
@@ -92,9 +117,9 @@ object Day08 : DayXX() {
     }
 
     private fun checkLeftScore(list: MutableList<MutableList<Int>>, i: Int, j: Int, original: Int): Int {
-        val previousHeights = mutableListOf<Int>()
-
         if (j == 0) return 0
+
+        val previousHeights = mutableListOf<Int>()
 
         for (idx in j - 1 downTo 0) {
             val current = list[i][idx]
@@ -110,9 +135,9 @@ object Day08 : DayXX() {
     }
 
     private fun checkRightScore(list: MutableList<MutableList<Int>>, i: Int, j: Int, original: Int): Int {
-        val previousHeights = mutableListOf<Int>()
-
         if (j + 1 == list[i].size) return 0
+
+        val previousHeights = mutableListOf<Int>()
 
         for (idx in j + 1 until list[i].size) {
             val current = list[i][idx]
@@ -125,9 +150,6 @@ object Day08 : DayXX() {
         }
 
         return previousHeights.size
-    }
-
-    override fun part2() {
     }
 }
 
