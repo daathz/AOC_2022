f5579e5eb9122c3d2c985b613c0a6121bb432871
Novák Viktor <novk.viktor@gmail.com>
2022-12-14 10:59:33 +0100

    day14 refactor

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,54 +1,75 @@
 data class Coordinate(val x: Int, val y: Int)
 object Day14 : DayXX() {
     override fun part1() {
+        val rockScans = getRockScans(readInput("day14"))
+
+        val rockCoordinates = getRockCoordinates(rockScans)
+
+        val highestY = rockCoordinates.map { it.y }.max()
+        var count = -1 // need to start from -1 cause last append is not valid
+        do {
+            val moveSand = moveSand(rockCoordinates, highestY)
+            count++
+        } while (moveSand)
+        println(count)
     }
 
     override fun part2() {
-        val rockScans = readInput("day14").map { line -> line.split(" -> ") }
-            .map { pairs -> pairs.map { pair -> pair.split(",").map { s -> s.toInt() } } }
+        val rockScans = getRockScans(readInput("day14"))
 
+        val rockCoordinates = getRockCoordinates(rockScans)
+
+        val highestY = rockCoordinates.map { it.y }.max()
+        var count = 0
+        do {
+            val moveSand = moveSandUntilStartIsFilled(rockCoordinates, highestY + 2)
+            count++
+        } while (moveSand)
+        println(count)
+    }
+
+    private fun getRockScans(input: List<String>): List<List<Coordinate>> = input.map { line -> line.split(" -> ") }
+        .map { moves ->
+            moves.map { move -> move.split(",") }
+                .map { move -> Coordinate(move[0].toInt(), move[1].toInt()) }
+        }
+
+    private fun getRockCoordinates(rockScans: List<List<Coordinate>>): MutableSet<Coordinate> {
         val rockCoordinates = mutableSetOf<Coordinate>()
 
         rockScans.forEach { rockScan ->
             for (i in rockScan.indices) {
                 if (i == 0) {
-                    rockCoordinates.add(Coordinate(rockScan[i][0], rockScan[i][1]))
+                    rockCoordinates.add(Coordinate(rockScan[i].x, rockScan[i].y))
                 } else {
                     val currentMove = rockScan[i]
                     val prevMove = rockScan[i - 1]
-                    if (prevMove[0] < currentMove[0]) {
-                        for (j in prevMove[0] .. currentMove[0]) {
-                            rockCoordinates.add(Coordinate(j, currentMove[1]))
+                    when {
+                        prevMove.x < currentMove.x -> {
+                            (prevMove.x .. currentMove.x).forEach { xIndex ->
+                                rockCoordinates.add(Coordinate(xIndex, currentMove.y)) }
                         }
-                    } else if (prevMove[0] > currentMove[0]) {
-                        for (j in prevMove[0]downTo currentMove[0]) {
-                            rockCoordinates.add(Coordinate(j, currentMove[1]))
+                        prevMove.x > currentMove.x -> {
+                            (prevMove.x downTo currentMove.x).forEach { xIndex ->
+                                rockCoordinates.add(Coordinate(xIndex, currentMove.y))
+                            }
                         }
-                    } else if (prevMove[1] < currentMove[1]) {
-                        for (j in prevMove[1]..currentMove[1]) {
-                            rockCoordinates.add(Coordinate(currentMove[0], j))
+                        prevMove.y < currentMove.y -> {
+                            (prevMove.y..currentMove.y).forEach { yIndex ->
+                                rockCoordinates.add(Coordinate(currentMove.x, yIndex))
+                            }
                         }
-                    } else if (prevMove[1] > currentMove[1]) {
-                        for (j in prevMove[1] downTo currentMove[1]) {
-                            rockCoordinates.add(Coordinate(currentMove[0], j))
+                        prevMove.y > currentMove.y -> {
+                            (prevMove.y downTo currentMove.y).forEach { yIndex ->
+                                rockCoordinates.add(Coordinate(currentMove.x, yIndex))
+                            }
                         }
                     }
                 }
             }
         }
 
-        val highestY = rockCoordinates.map { it.y }.max()
-        val lowestX = rockCoordinates.map { it.x }.min()
-        val highestX = rockCoordinates.map { it.x }.max()
-        for (i in lowestX..highestX) {
-            rockCoordinates.add(Coordinate(i, highestY + 2))
-        }
-        var count = 0
-        do {
-            val moveSand = moveSand(rockCoordinates, highestY + 2)
-            count++
-        } while (moveSand)
-        println(count)
+        return rockCoordinates
     }
 
     private fun moveSand(rockCoordinates: MutableSet<Coordinate>, highestY: Int): Boolean {
@@ -59,15 +80,39 @@ object Day14 : DayXX() {
             val downLeftCoordinate = Coordinate(sandCoordinate.x - 1, sandCoordinate.y + 1)
             val downRightCoordinate = Coordinate(sandCoordinate.x + 1, sandCoordinate.y + 1)
 
-            if (!rockCoordinates.contains(downCoordinate) && downCoordinate.y < highestY) {
-                sandCoordinate = Coordinate(downCoordinate.x, downCoordinate.y)
-            } else if (!rockCoordinates.contains(downLeftCoordinate) && downLeftCoordinate.y < highestY) {
-                sandCoordinate = downLeftCoordinate
-            } else if (!rockCoordinates.contains(downRightCoordinate) && downLeftCoordinate.y < highestY) {
-                sandCoordinate = downRightCoordinate
-            } else {
-                rockCoordinates.add(sandCoordinate)
-                return sandCoordinate != Coordinate(500, 0)
+            sandCoordinate = when {
+                !rockCoordinates.contains(downCoordinate) -> downCoordinate
+                !rockCoordinates.contains(downLeftCoordinate) -> downLeftCoordinate
+                !rockCoordinates.contains(downRightCoordinate) -> downRightCoordinate
+                else -> {
+                    rockCoordinates.add(sandCoordinate)
+                    return true
+                }
+            }
+
+            if (sandCoordinate.y >= highestY) return false
+        }
+    }
+
+    private fun moveSandUntilStartIsFilled(rockCoordinates: MutableSet<Coordinate>, highestY: Int): Boolean {
+        var sandCoordinate = Coordinate(500, 0)
+
+        while (true) {
+            val downCoordinate = Coordinate(sandCoordinate.x, sandCoordinate.y + 1)
+            val downLeftCoordinate = Coordinate(sandCoordinate.x - 1, sandCoordinate.y + 1)
+            val downRightCoordinate = Coordinate(sandCoordinate.x + 1, sandCoordinate.y + 1)
+
+            sandCoordinate = when (downCoordinate.y < highestY) {
+                !rockCoordinates.contains(downCoordinate) -> { downCoordinate
+                }
+                !rockCoordinates.contains(downLeftCoordinate) -> { downLeftCoordinate
+                }
+                !rockCoordinates.contains(downRightCoordinate) -> { downRightCoordinate
+                }
+                else -> {
+                    rockCoordinates.add(sandCoordinate)
+                    return sandCoordinate != Coordinate(500, 0)
+                }
             }
         }
     }
