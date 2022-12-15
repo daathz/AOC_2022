6348631113804286712d51e1c88dae82d34bbd4a
Novák Viktor <novk.viktor@gmail.com>
2022-12-15 08:54:41 +0100

    day15 refactor

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,54 +1,84 @@
 import java.math.BigInteger
 import kotlin.math.abs
 
-data class LCoordinate(val x: Long, val y: Long)
-
-data class SignalPair(val sensor: LCoordinate, val beacon: LCoordinate) {
-    fun distance(): Long = abs(sensor.x - beacon.x) + abs(sensor.y - beacon.y)
+data class LCoordinate(val x: Long, val y: Long) {
+    fun distance(other: LCoordinate): Long = abs(x - other.x) + abs(y - other.y)
 }
+
+data class SignalPair(val sensor: LCoordinate, val beacon: LCoordinate)
+
 object Day15 : DayXX() {
 
-    private const val Y_LINE = 2000000L
+    private const val Y_LINE = 2_000_000L
+    private const val AREA_MAX = 4_000_000
+    private val freq = BigInteger(AREA_MAX.toString())
+
     override fun part1() {
-        /*val signalPairs = mutableSetOf<SignalPair>()
-        val map = mutableSetOf<LCoordinate>()
 
-        readInput("day15").map { it.split(" ") }
-            .forEach {
-                val sensorX = it[2].substring(2, it[2].length - 1).toLong()
-                val sensorY = it[3].substring(2, it[3].length - 1).toLong()
-
-                val beaconX = it[8].substring(2, it[8].length - 1).toLong()
-                val beaconY = it[9].substring(2, it[9].length).toLong()
-
-                map.add(LCoordinate(sensorX, sensorY))
-                map.add(LCoordinate(beaconX, beaconY))
-                signalPairs.add(SignalPair(LCoordinate(sensorX, sensorY), LCoordinate(beaconX, beaconY)))
-            }
+        val signalPairs = getSignalPairs(readInput("day15"))
 
         val signalMap = mutableSetOf<LCoordinate>()
-        signalPairs.forEach {
-            val distanceToY = abs(it.sensor.y - Y_LINE)
+        signalPairs.forEach { (sensor, beacon) ->
+            val distanceToY = abs(sensor.y - Y_LINE)
 
-            val width = it.distance() - distanceToY
+            val range = sensor.distance(beacon) - distanceToY
 
-            for (i in it.sensor.x - width..it.sensor.x + width) {
-                val c = LCoordinate(i, Y_LINE)
-                if (c != it.beacon) {
-                    signalMap.add(c)
+            (sensor.x - range..sensor.x + range).forEach { idx ->
+                val current = LCoordinate(idx, Y_LINE)
+                if (current != beacon) {
+                    signalMap.add(current)
                 }
             }
         }
 
         val signalsInRow = signalMap.count { it.y == Y_LINE }
-        println(signalsInRow)*/
+        println(signalsInRow)
     }
 
     override fun part2() {
-        val signalPairs = mutableSetOf<SignalPair>()
-        val map = mutableSetOf<LCoordinate>()
+        val signalPairs = getSignalPairs(readInput("day15"))
 
-        readInput("day15").map { it.split(" ") }
+        val rangesList = mutableListOf(mutableListOf<LongRange>())
+
+        repeat(AREA_MAX + 1) { rangesList.add(mutableListOf()) }
+
+        signalPairs.forEach { (sensor, beacon) ->
+            (0..AREA_MAX).forEach { lineY ->
+                val distanceToY = abs(sensor.y - lineY)
+
+                val range = sensor.distance(beacon) - distanceToY
+
+                if (range > 0) {
+                    val rangeFirst = maxOf(0, sensor.x - range)
+                    val rangeLast = minOf(AREA_MAX.toLong(), sensor.x + range)
+
+                    rangesList[lineY].add(rangeFirst..rangeLast)
+                }
+            }
+        }
+
+        rangesList.map { it.sortedBy { ranges -> ranges.first } }
+            .forEachIndexed { index, ranges ->
+                var endOfLastRange = ranges.first().last
+
+                (1 until ranges.size).forEach { idx ->
+                    val firstLongOfNewRange = ranges[idx].first
+
+                    when {
+                        firstLongOfNewRange > endOfLastRange -> {
+                            return println(getTuningFrequency(index, endOfLastRange + 1))
+                        }
+
+                        ranges[idx].last > endOfLastRange -> endOfLastRange = ranges[idx].last
+                    }
+                }
+            }
+    }
+
+    private fun getSignalPairs(input: List<String>): MutableSet<SignalPair> {
+        val signalPairs = mutableSetOf<SignalPair>()
+
+        input.map { it.split(" ") }
             .forEach {
                 val sensorX = it[2].substring(2, it[2].length - 1).toLong()
                 val sensorY = it[3].substring(2, it[3].length - 1).toLong()
@@ -56,41 +86,14 @@ object Day15 : DayXX() {
                 val beaconX = it[8].substring(2, it[8].length - 1).toLong()
                 val beaconY = it[9].substring(2, it[9].length).toLong()
 
-                map.add(LCoordinate(sensorX, sensorY))
-                map.add(LCoordinate(beaconX, beaconY))
                 signalPairs.add(SignalPair(LCoordinate(sensorX, sensorY), LCoordinate(beaconX, beaconY)))
             }
 
-        val rangesList = mutableListOf(mutableListOf<LongRange>())
-        repeat((0..4_000_000).count()) { rangesList.add(mutableListOf()) }
-        signalPairs.forEach {
-            for (i in 0..4_000_000) {
-                val distanceToY = abs(it.sensor.y - i)
+        return signalPairs
+    }
 
-                val width = it.distance() - distanceToY
-                if (width > 0) {
-                    rangesList[i].add(it.sensor.x - width..it.sensor.x + width)
-                }
-            }
-        }
-
-        val freq = BigInteger("4_000_000")
-        rangesList.forEachIndexed { index, ranges ->
-            val sortedRanges = ranges.sortedBy { it.first }
-            var endOfLastRange = sortedRanges.first().last
-
-            for (i in 1 until sortedRanges.size) {
-                val firstLongOfNewRange = sortedRanges[i].first
-                if (firstLongOfNewRange > endOfLastRange) {
-                    println((firstLongOfNewRange - 1).toBigInteger() * freq + index.toBigInteger())
-                    return
-                } else if (sortedRanges[i].last > endOfLastRange) {
-                    endOfLastRange = sortedRanges[i].last
-                }
-            }
-        }
-
-        println()
+    private fun getTuningFrequency(index: Int, firstLongOfNewRange: Long): BigInteger {
+        return firstLongOfNewRange.toBigInteger() * freq + index.toBigInteger()
     }
 }
 
