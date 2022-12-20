8f1d723a83e2126b7878bb4d9bdc302cb7597ffa
Novák Viktor <novk.viktor@gmail.com>
2022-12-20 07:23:34 +0100

    day20 part 2

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -1,18 +1,22 @@
 object Day20 : DayXX() {
+    const val DECRYPTION_KEY = 811589153
+
     override fun part1() {
-        val encrypted = readInput("day20").map { it.toInt() }
+        val encrypted = readInput("day20").map { it.toLong() * DECRYPTION_KEY }
             .mapIndexed { idx, value ->
                 Seq(idx, value)
             }
 
         var decrypted = encrypted.toMutableList()
 
-        encrypted.forEach { value ->
-            val idx = decrypted.indexOf(value)
-            decrypted.removeAt(idx)
-            decrypted.add((idx + value.value).mod(decrypted.size), value) // mod equals abs + %
+        repeat(10) {
+            encrypted.forEach { value ->
+                val idx = decrypted.indexOf(value)
+                decrypted.removeAt(idx)
+                decrypted.add((idx + value.value).mod(decrypted.size), value) // mod equals abs + %
 
-            // println(decrypted.map { it.value }.joinToString())
+                // println(decrypted.map { it.value }.joinToString())
+            }
         }
 
         // println(decrypted.map { it.value }.joinToString())
@@ -33,4 +37,4 @@ fun main() {
     Day20.solve()
 }
 
-data class Seq(val index: Int, val value: Int)
+data class Seq(val index: Int, val value: Long)
