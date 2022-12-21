f2ff38c4e9fde7f7160beabe172fe4cf404547b3
Novák Viktor <novk.viktor@gmail.com>
2022-12-21 06:25:32 +0100

    day21 part 1

---
+++ src/main/kotlin/Day21.kt
@@ -0,0 +1,71 @@
+object Day21 : DayXX() {
+    override fun part1() {
+        var jobs = readInput("day21").map { it.split(": ") }.map { line ->
+            val name = line[0]
+            val isNumeric = isNumeric(line[1])
+            val job = if (!isNumeric) line[1] else ""
+            val number = if (isNumeric) line[1].toLong() else 0L
+
+            Job(name, job, number)
+        }
+
+        // jobs.forEach(::println)
+
+        while (jobs.first { it.name == "root" }.job != "") {
+            jobs = calc(jobs)
+        }
+        println(jobs.first { it.name == "root" }.number)
+    }
+
+    override fun part2() {
+    }
+
+    private fun calc(jobs: List<Job>): List<Job> {
+        val newJobs = jobs.toMutableList()
+
+        for (currentJob in jobs) {
+            if (currentJob.number != 0L) {
+                val name = currentJob.name
+                val number = currentJob.number
+
+                for (newJob in newJobs) {
+                    if (newJob.job != "" && newJob.job.contains(name)) {
+                        newJob.job = newJob.job.replace(name, number.toString())
+
+                        val operator = newJob.job.split(" ")
+                        if (isNumeric(operator[0]) && isNumeric(operator[2])) {
+                            val left = operator[0].toLong()
+                            val right = operator[2].toLong()
+
+                            val newNumber = when (operator[1]) {
+                                "+" -> left + right
+                                "-" -> left - right
+                                "*" -> left * right
+                                "/" -> left / right
+                                else -> 0
+                            }
+
+                            newJob.job = ""
+                            newJob.number = newNumber
+
+                            // println(newJob)
+                        }
+                    }
+                }
+            }
+        }
+
+        return newJobs
+    }
+
+    private fun isNumeric(toCheck: String): Boolean {
+        val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+        return toCheck.matches(regex)
+    }
+}
+
+fun main() {
+    Day21.solve()
+}
+
+data class Job(val name: String, var job: String, var number: Long)
