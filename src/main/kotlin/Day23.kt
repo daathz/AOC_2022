83ea254358ffea53364c4c8bcbcd0b8b0c342540
Viktor Novák <novk.viktor@gmail.com>
2022-12-23 07:05:30 +0100

    day23 part 1

---
+++ src/main/kotlin/Day23.kt
@@ -0,0 +1,178 @@
+object Day23 : DayXX() {
+    override fun part1() {
+        val elfCoordinates = mutableSetOf<Coordinate>()
+        readInput("day23").forEachIndexed { lIndex, line ->
+            line.forEachIndexed { cIndex, c ->
+                if (c == '#') elfCoordinates.add(Coordinate(lIndex, cIndex))
+            }
+        }
+        var direction = 0
+
+        repeat(10) {
+            var moves = mutableSetOf<Pair<Coordinate, Coordinate>>()
+            elfCoordinates.forEach { elf ->
+                if (!elf.checkSurrounding(elfCoordinates)) {
+                    when (direction) {
+                        0 -> {
+                            if (elf.proposeNorth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x - 1, elf.y))
+                            } else if (elf.proposeSouth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x + 1, elf.y))
+                            } else if (elf.proposeWest(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y - 1))
+                            } else if (elf.proposeEast(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y + 1))
+                            }
+                        }
+                        1 -> {
+                            if (elf.proposeSouth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x + 1, elf.y))
+                            } else if (elf.proposeWest(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y - 1))
+                            } else if (elf.proposeEast(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y + 1))
+                            } else if (elf.proposeNorth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x - 1, elf.y))
+                            }
+                        }
+                        2 -> {
+                            if (elf.proposeWest(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y - 1))
+                            } else if (elf.proposeEast(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y + 1))
+                            } else if (elf.proposeNorth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x - 1, elf.y))
+                            } else if (elf.proposeSouth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x + 1, elf.y))
+                            }
+                        }
+                        else -> {
+                            if (elf.proposeEast(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y + 1))
+                            } else if (elf.proposeNorth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x - 1, elf.y))
+                            } else if (elf.proposeSouth(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x + 1, elf.y))
+                            } else if (elf.proposeWest(elfCoordinates)) {
+                                moves.add(elf to Coordinate(elf.x, elf.y - 1))
+                            }
+                        }
+                    }
+                }
+            }
+
+            val inspected = mutableListOf<Coordinate>()
+            val duplicate = mutableListOf<Coordinate>()
+            moves.forEach { move ->
+                if (!inspected.contains(move.second)) {
+                    inspected.add(move.second)
+                } else  duplicate.add(move.second)
+            }
+
+            moves = moves.filter { !duplicate.contains(it.second) }.toMutableSet()
+
+            moves.forEach { move ->
+                elfCoordinates.remove(move.first)
+                elfCoordinates.add(move.second)
+            }
+
+            direction = (direction + 1) % 4
+
+            val minX = elfCoordinates.map { it.x }.min()
+            val maxX = elfCoordinates.map { it.x  }.max()
+            val minY = elfCoordinates.map { it.y }.min()
+            val maxY = elfCoordinates.map { it.y  }.max()
+
+            println("=== MOVE ===")
+            for (x in minX..maxX) {
+                for (y in minY..maxY) {
+                    if (elfCoordinates.contains(Coordinate(x, y))) print('#') else print('.')
+                }
+                println()
+            }
+            println()
+        }
+
+        val minX = elfCoordinates.map { it.x }.min()
+        val maxX = elfCoordinates.map { it.x  }.max()
+        val minY = elfCoordinates.map { it.y }.min()
+        val maxY = elfCoordinates.map { it.y  }.max()
+
+        var count = 0
+        for (x in minX..maxX) {
+            for (y in minY..maxY) {
+                if (!elfCoordinates.contains(Coordinate(x, y))) {
+                    count++
+                }
+            }
+        }
+
+        println(count)
+    }
+
+    override fun part2() {
+    }
+}
+
+fun Coordinate.checkSurrounding(elfCoordinates: Set<Coordinate>): Boolean {
+    val northWest = Coordinate(this.x - 1, this.y - 1)
+    val north = Coordinate(this.x - 1, this.y)
+    val northEast = Coordinate(this.x - 1, this.y + 1)
+    val west = Coordinate(this.x, this.y - 1)
+    val east = Coordinate(this.x, this.y + 1)
+    val southWest = Coordinate(this.x + 1, this.y - 1)
+    val south = Coordinate(this.x + 1, this.y)
+    val southEast = Coordinate(this.x + 1, this.y + 1)
+
+    val surrounding = setOf(
+        northWest, northEast, north, west, east, southWest, south, southEast
+    )
+
+    return surrounding.subtract(elfCoordinates).size == 8
+}
+
+fun Coordinate.proposeNorth(elfCoordinates: Set<Coordinate>): Boolean {
+    val northCoordinates = setOf(
+        Coordinate(this.x - 1, this.y - 1),
+        Coordinate(this.x - 1, this.y),
+        Coordinate(this.x - 1, this.y + 1)
+    )
+
+    return northCoordinates.subtract(elfCoordinates).size == 3
+}
+
+fun Coordinate.proposeSouth(elfCoordinates: Set<Coordinate>): Boolean {
+    val southCoordinates = setOf(
+        Coordinate(this.x + 1, this.y - 1),
+        Coordinate(this.x + 1, this.y),
+        Coordinate(this.x + 1, this.y + 1)
+    )
+
+    return southCoordinates.subtract(elfCoordinates).size == 3
+}
+
+fun Coordinate.proposeWest(elfCoordinates: Set<Coordinate>): Boolean {
+    val westCoordinates = setOf(
+        Coordinate(this.x, this.y - 1),
+        Coordinate(this.x - 1, this.y - 1),
+        Coordinate(this.x + 1, this.y - 1)
+    )
+
+    return westCoordinates.subtract(elfCoordinates).size == 3
+}
+
+fun Coordinate.proposeEast(elfCoordinates: Set<Coordinate>): Boolean {
+    val eastCoordinates = setOf(
+        Coordinate(this.x, this.y + 1),
+        Coordinate(this.x - 1, this.y + 1),
+        Coordinate(this.x + 1, this.y + 1)
+    )
+
+    return eastCoordinates.subtract(elfCoordinates).size == 3
+}
+
+
+
+fun main() {
+    Day23.solve()
+}
