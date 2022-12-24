892a18b27889a8d844d172d2334796b102136c13
Viktor Novák <novk.viktor@gmail.com>
2022-12-24 09:33:29 +0100

    day24 refactor

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -1,112 +1,39 @@
 object Day24 : DayXX() {
+
     override fun part1() {
-        var initMap = mutableSetOf<Square>()
-        readInput("day24").forEachIndexed { lIndex,line ->
-            line.forEachIndexed { cIndex, c ->
-                initMap.add(Square(lIndex toY cIndex,  c))
-            }
-        }
+        val (map, endPosition) = getMapAndEndPosition(readInput("day24"))
 
-        val goal = initMap.last { it.type == '.' }.coordinate
+        val startPosition = 0 toY 1
 
-        initMap = initMap.filter { it.type != '.' }.toMutableSet()
-
-        var map = initMap.toSet()
-
-        var time = 0
-        // var deque = ArrayDeque<BlizzardState>()
-        val maxX = initMap.maxOf { it.coordinate.x }
-        val maxY = initMap.maxOf { it.coordinate.y }
-        val start = 0 toY 1
-        // deque.add(BlizzardState(initMap.toSet(), 0 toY 1, 0))
-
-        /*for (i in 0 .. 18) {
-            println("*** $i ***")
-            for (x in 0..maxX) {
-                for (y in 0..maxY) {
-                    val squareCount = initMap.count { it.coordinate == x toY y }
-                    if (squareCount > 1) print(squareCount)
-                    else if (squareCount == 1) print(initMap.first { it.coordinate == x toY y }.type)
-                    else print('.')
-                }
-                println()
-            }
-            initMap = moveBlizzards(initMap) as MutableSet<Square>
-            println()
-        }*/
-
-        val currentPositions = mutableSetOf(start)
-        val directions = listOf(1 toY 0, 0 toY 1, -1 toY 0, 0 toY -1, 0 toY 0)
-        while (true) {
-            if (currentPositions.contains(goal)) break
-
-            time++
-
-            map = moveBlizzards(map)
-            val nextPositions = mutableSetOf<Coordinate>()
-            for (position in currentPositions) {
-                for (direction in directions) {
-                    val newPos = position + direction
-                    if (newPos.x >= 0 && newPos.y >= 0 && newPos.x <= maxX && newPos.y <= maxY) {
-                        if (map.find { it.coordinate == newPos } == null) {
-                            nextPositions.add(newPos)
-                        }
-                    }
-                }
-            }
-            currentPositions.clear()
-            currentPositions.addAll(nextPositions)
-        }
-
-        currentPositions.clear()
-        currentPositions.add(goal)
-        while (true) {
-            if (currentPositions.contains(start)) break
-
-            time++
-
-            map = moveBlizzards(map)
-            val nextPositions = mutableSetOf<Coordinate>()
-            for (position in currentPositions) {
-                for (direction in directions) {
-                    val newPos = position + direction
-                    if (newPos.x >= 0 && newPos.y >= 0 && newPos.x <= maxX && newPos.y <= maxY) {
-                        if (map.find { it.coordinate == newPos } == null) {
-                            nextPositions.add(newPos)
-                        }
-                    }
-                }
-            }
-            currentPositions.clear()
-            currentPositions.addAll(nextPositions)
-        }
-        currentPositions.clear()
-        currentPositions.add(start)
-        while (true) {
-            if (currentPositions.contains(goal)) break
-
-            time++
-
-            map = moveBlizzards(map)
-            val nextPositions = mutableSetOf<Coordinate>()
-            for (position in currentPositions) {
-                for (direction in directions) {
-                    val newPos = position + direction
-                    if (newPos.x >= 0 && newPos.y >= 0 && newPos.x <= maxX && newPos.y <= maxY) {
-                        if (map.find { it.coordinate == newPos } == null) {
-                            nextPositions.add(newPos)
-                        }
-                    }
-                }
-            }
-            currentPositions.clear()
-            currentPositions.addAll(nextPositions)
-        }
+        val (time, _) = moveFromStartToGoal(startPosition, endPosition, map)
 
         println(time)
     }
 
     override fun part2() {
+        val (map, endPosition) = getMapAndEndPosition(readInput("day24"))
+
+        val startPosition = 0 toY 1
+
+        val (time1, map1) = moveFromStartToGoal(startPosition, endPosition, map)
+        val (time2, map2) = moveFromStartToGoal(endPosition, startPosition, map1)
+        val (time3, _) = moveFromStartToGoal(startPosition, endPosition, map2)
+
+        println(time1 + time2 + time3)
+    }
+
+    private fun getMapAndEndPosition(input: List<String>): Pair<Set<Square>, Coordinate> {
+        val mapWithAllElements = input.flatMapIndexed { lIndex, line ->
+            line.mapIndexed { cIndex, c ->
+                Square(lIndex toY cIndex, c)
+            }
+        }
+
+        val map = mapWithAllElements.filter { it.type != '.' }.toSet()
+
+        val goal = mapWithAllElements.last { it.type == '.' }.coordinate
+
+        return map to goal
     }
 
     private fun moveBlizzards(map: Set<Square>): Set<Square> {
@@ -132,6 +59,55 @@ object Day24 : DayXX() {
         map.forEach { square -> result.add(square.copy()) }
         return result.toSet()
     }
+
+    private fun moveFromStartToGoal(start: Coordinate, goal: Coordinate, map: Set<Square>): Pair<Int, Set<Square>> {
+        val maxX = map.maxOf { it.coordinate.x }
+        val maxY = map.maxOf { it.coordinate.y }
+        val directions = listOf(1 toY 0, 0 toY 1, -1 toY 0, 0 toY -1, 0 toY 0)
+
+        var time = 0
+        val currentPositions = mutableSetOf(start)
+        var currentMap = map.map { it }.toSet()
+        while (true) {
+            if (currentPositions.contains(goal)) break
+
+            time++
+
+            currentMap = moveBlizzards(currentMap)
+            val nextPositions = mutableSetOf<Coordinate>()
+            for (position in currentPositions) {
+                // println("$time $position")
+                for (direction in directions) {
+                    val newPos = position + direction
+                    if (newPos.x >= 0 && newPos.y >= 0 && newPos.x <= maxX && newPos.y <= maxY) {
+                        if (currentMap.find { it.coordinate == newPos } == null) {
+                            nextPositions.add(newPos)
+                        }
+                    }
+                }
+            }
+            currentPositions.clear()
+            currentPositions.addAll(nextPositions)
+        }
+
+        return time to currentMap
+    }
+
+    private fun printMap(map: Set<Square>) {
+        val maxX = map.maxOf { it.coordinate.x }
+        val maxY = map.maxOf { it.coordinate.y }
+
+        for (x in 0..maxX) {
+            for (y in 0..maxY) {
+                val squareCount = map.count { it.coordinate == x toY y }
+                if (squareCount > 1) print(squareCount)
+                else if (squareCount == 1) print(map.first { it.coordinate == x toY y }.type)
+                else print('.')
+            }
+            println()
+        }
+        println()
+    }
 }
 
 fun main() {
@@ -139,5 +115,3 @@ fun main() {
 }
 
 data class Square(var coordinate: Coordinate, val type: Char)
-
-data class BlizzardState(val map: Set<Square>, val myCoordinate: Coordinate, val minute: Int)
