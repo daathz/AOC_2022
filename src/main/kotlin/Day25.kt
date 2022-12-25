a4b413060235f5d459b159d5434fe3d58bbd07a4
Viktor Novák <novk.viktor@gmail.com>
2022-12-25 07:07:39 +0100

    day25 refactor

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -1,64 +1,53 @@
+import kotlin.math.pow
+
 object Day25 : DayXX() {
     override fun part1() {
-        val inputs = readInput("day25")
-
-        val sum = inputs.sumOf {
-            SNAFUToDecimal(it)
-        }.toLong()
+        val sum = readInput("day25").sumOf { snafu ->
+            snafuToDecimal(snafu)
+        }
 
         println(sum)
-        println(DecimalToSNAFU(sum))
+        println(decimalToSnafu(sum))
     }
 
     override fun part2() {
     }
 
-    private fun SNAFUToDecimal(number: String): Double {
-        val size = number.length
-
-        var result = 0.0
-        for ((idx, n) in number.withIndex()) {
-            if (n.isDigit()) {
-                result += n.toString().toDouble() * Math.pow((5).toDouble(), (size - idx - 1).toDouble())
-            } else if (n == '-') {
-                result += (-1).toDouble() * Math.pow((5).toDouble(), (size - idx - 1).toDouble())
-            } else {
-                result += (-2).toDouble() * Math.pow((5).toDouble(), (size - idx - 1).toDouble())
-            }
+    private fun snafuToDecimal(snafu: String): Long = snafu.mapIndexed { index, n ->
+        val number = when (n) {
+            '0' -> 0
+            '1' -> 1
+            '2' -> 2
+            '-' -> -1
+            '=' -> -2
+            else -> throw RuntimeException()
         }
 
-        return result
-    }
+        number * (5.0).pow((snafu.length - index - 1.0))
+    }.sum().toLong()
 
-    private fun DecimalToBase5(number: Int): String {
-        val numbers = mutableListOf<Int>()
-        var value = number
-
-        while (value != 0) {
-            numbers.add(value % 5)
-            value /= 5
-        }
-
-        return numbers.reversed().joinToString("")
-    }
-
-    private fun DecimalToSNAFU(number: Long): String {
+    private fun decimalToSnafu(decimal: Long): String {
         val numbers = mutableListOf<String>()
-        var value = number
+        var remaining = decimal
 
-        while (value != 0L) {
-            val divided = value % 5
-            if (divided <= 2) {
-                numbers.add(divided.toString())
-            } else if (divided == 3L) {
-                numbers.add("=")
-                value += 2
-            } else {
-                numbers.add("-")
-                value += 1
+        while (remaining != 0L) {
+            val divided = remaining % 5
+
+            when {
+                divided <= 2 -> {
+                    numbers.add(divided.toString())
+                }
+                divided == 3L -> {
+                    numbers.add("=")
+                    remaining += 2
+                }
+                else -> {
+                    numbers.add("-")
+                    remaining += 1
+                }
             }
 
-            value /= 5
+            remaining /= 5
         }
 
         return numbers.reversed().joinToString("")
